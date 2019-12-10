3cfa5f7918675898c1f0b47e5a7490e91e5a3656
IGOLTA <IGOLTA@DESKTOP-UDGJI01>
2019-12-10 14:19:49 +0100

    Removing test class because it was a bad idea. XD

--- simple-updater/src/main/java/fr/igolta/simple_updater/TestClass.java
+++
@@ -1,8 +0,0 @@
-package fr.igolta.simple_updater;
-
-public class TestClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Test build for simple updater\n----------------------");
-	}
-}
