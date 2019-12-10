0953f300f356b46cf1804bb18b173824e0e59d41
IGOLTA <IGOLTA@DESKTOP-UDGJI01>
2019-12-10 14:14:29 +0100

    Adding test class

---
+++ simple-updater/src/main/java/fr/igolta/simple_updater/TestClass.java
@@ -0,0 +1,8 @@
+package fr.igolta.simple_updater;
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Test build for simple updater\n----------------------");
+	}
+}
