08a41574c811d7505d329f43a584399f4afc4388
IGOLTA <IGOLTA@DESKTOP-UDGJI01>
2019-12-12 09:49:01 +0100

    Removed javadoc

--- simple-updater/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"fr.igolta.simple_updater"}]
--- simple-updater/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"fr.igolta.simple_updater","l":"MainTest"},{"p":"fr.igolta.simple_updater","l":"SimpleUpdater"}]
